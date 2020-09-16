12c48d57b933176a9cd08595ad950f96149cb874
NadzaVrazalica <69157486+NadzaVrazalica@users.noreply.github.com>
2020-09-16 09:15:46 +0200

    Update terragrunt-apply.sh

--- scripts/terragrunt-apply.sh
+++ scripts/terragrunt-apply.sh
@@ -8,7 +8,7 @@ set -euo pipefail
 LOG_FILE_NAME=${1:-plan.log}
 
 # Run apply all and display output both to terminal and the log file temp.log
-terragrunt apply-all --terragrunt-working-dir live/${ENVIRONMENT} --terragrunt-non-interactive 2>&1 | tee temp.log
+terragrunt apply-all --terragrunt-working-dir live/dev --terragrunt-non-interactive 2>&1 | tee temp.log
 
 # Remove bash colors from log file and put the output into the new log file
 sed -r "s/\x1B\[([0-9]{1,3}((;[0-9]{1,3})*)?)?[m|K]//g" temp.log > ${LOG_FILE_NAME}
